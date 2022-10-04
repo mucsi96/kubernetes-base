19e92eb6d50df37377ae65552b2afaacd2bda0c0
Igor Bari <mucsi96@gmail.com>
2022-10-04 08:14:54 +0200

    move modules

--- main.tf
+++ main.tf
@@ -1,23 +1,29 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
 module "cert-manager" {
-  source = "./modules/cert-manager"
+  source = "./cert-manager"
 }
 
 module "sealed-secrets" {
-  source = "./modules/sealed-secrets"
+  source = "./sealed-secrets"
 }
 
 module "kubernetes-dashboard" {
-  source    = "./modules/kubernetes-dashboard"
+  source    = "./kubernetes-dashboard"
   namespace = "monitoring"
 }
 
 module "spring-boot-admin" {
-  source     = "./modules/spring-boot-admin"
+  source     = "./spring-boot-admin/deploy"
   namespace  = "monitoring"
   image_name = "mucsi96/spring-boot-admin-server"
 }
 
 module "monitoring" {
-  source    = "./modules/monitoring"
+  source    = "./monitoring"
   namespace = "monitoring"
 }
