92788615754cf350829c0b2a1ad619817d7db3e6
Igor Bari <mucsi96@gmail.com>
2022-10-06 21:56:07 +0200

    move cluster issuer

--- cert-manager.tf
+++ cert-manager.tf
@@ -11,3 +11,24 @@ resource "helm_release" "cert-manager" {
     value = "true"
   }
 }
+
+# CRD Ref: https://cert-manager.io/docs/reference/api-docs/#cert-manager.io/v1.ClusterIssuer
+resource "kubernetes_manifest" "tls_issuer" {
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+    metadata = {
+      name = "tls-issuer"
+    }
+    spec = {
+      acme = {
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+        email  = "mucsi96@gmail.com"
+        privateKeySecretRef = {
+          name = "tls-secret"
+        }
+        solvers = [{ http01 = { ingress = { class = "traefik" } } }]
+      }
+    }
+  }
+}
